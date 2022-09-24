60ca1db8e0e99480a2ee386d254d855132ac1bc0
rushi7255 <88275343+rushi7255@users.noreply.github.com>
2022-09-24 15:04:00 +0530

    Add files via upload

---
+++ forloop1.java
@@ -0,0 +1,51 @@
+class forloop1
+{
+public static void main(String a[])
+{
+// for (initialization; condition;increment)
+
+//for(int i=1;i<=5;i++)   System.out.println("hello");
+
+//for(int i=10;i<=15;i++)   System.out.println("hello");
+
+//for(int i=10;i<=15;i=i+1)   System.out.println("hello");
+
+
+//for(;;)   System.out.println("infinite times the loop will run");
+
+//for(int p=10;p>=1;p--)   System.out.println("decrement");
+
+
+//for( int k=1;k<=10;k--)     System.out.println(k); 
+
+/*int y=10;
+for(; y<=20;)
+{
+ System.out.println(" testing block" +y);
+ y++;//without this line thic loop will become infinite 
+}
+*/
+
+for(int j=0,k=5,r=3     ; k<=10          ; j++,r++,k++)
+{
+  System.out.println("j="+  j+  " k =  "  + k +  " r= "+ r);
+}
+
+}}
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
